5017aabfdc63840f36e7d9b1b7208990b9283ea5
rockshellS <spicer.rachelle@gmail.com>
2021-02-25 19:53:14 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+
+import React, { Component } from 'react'
+
+
+
+
+class EyesOnMe extends Component {
+    constructor() {
+        super()
+    }
+
+ 
+render() {
+     return(
+        <div>
+            <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>BUTTON </button>
+        </div>         
+     )
+ }
+}
+ export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+    constructor() {
+        super() 
+    }
+
+
+
+render() {
+    return(
+            <div>
+                <input type="password" onKeyUp={() => console.log('Entering password...')}/>
+            </div>
+    )
+}
+}
+
+export default Keypad
